6f541f658785db7c4af69d8956de8d04bfa91b10
abselyam15 <99846458+abselyam15@users.noreply.github.com>
2024-02-08 21:40:13 +0100

    Fix pathes

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,9 @@
-// function slugify(title) {
-//     const slug = title.toLowerCase()
-//     const replacedSlug = slug.split(" ").join("-")
+function slugify(title) {
+    const slug = title.toLowerCase()
+    return slug.split(" ").join("-")
+}
 
-//     return replacedSlug
-// }
-
-// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-// console.log(slugify("English for developer")); // "english-for-developer"
-// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log("[TASK 1]: " + slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log("[TASK 1]: " + slugify("English for developer")); // "english-for-developer"
+console.log("[TASK 1]: " + slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log("[TASK 1]: " + slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
